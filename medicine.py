19f5dbb87a3386932a6f61905a18dbfba401f037
owino600 <owinodaniel600@gmail.com>
2024-02-10 17:23:02 +0300

    .\medicine.py

--- medicine.py
+++ medicine.py
@@ -1,19 +1,17 @@
 #!/bin/usr/python3
 """ Medicine progress"""
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Medicine():
     def __init__(self, med_type, issue_date, issue_duration, issue_complete):
         self.med_type = med_type
         self.date_given = datetime.utcnow()
         self.duration = issue_duration
-        self.med_end = issue_complete
+        self.med_end = self.date_given + timedelta(days=self.duration)
         
 if __name__ == "__main__":
     med_type = input("Medicine Issued: ")
-    date_given = input("Date of issue: ")
     duration = input("Medication Duration: ")
-    med_end = input("Medication Complete date: ")
-        
+    
     health = Medicine(med_type, date_given, duration, med_end)
     print("medication data saved!")
