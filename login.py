bbdc32cae0d609b01f9c91aa4547320477d42cf1
owino600 <owinodaniel600@gmail.com>
2024-02-10 16:09:50 +0300

    .\login.py

---
+++ login.py
@@ -0,0 +1,16 @@
+#!/bin/usr/python3
+class User_sign():
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+        if __name__ == "__main__":
+            print("Starting program...")
+            username = input("Enter Username: ")
+            email = input("Enter Email: ")
+            password = input("Enter Password: ")
+            print("Inputs collected...")
+        
+        create_account = User_sign(username, email, password)
+        print("User account created...")
+        print("hello, welcome to kuku-poa")
