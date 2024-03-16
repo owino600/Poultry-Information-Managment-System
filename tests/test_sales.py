53e4bae26688da450547be4bea5b0b4756478b97
owino600 <owinodaniel600@gmail.com>
2024-03-16 21:24:57 +0300

    .\test_sales.py

---
+++ tests/test_sales.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+from models.sales import Sales
+
+class TestSales(unittest.TestCase):
+    def setUp(self):
+        self.sale = Sales(Sales_id=1, buyer_name='John Doe', poultry_id=101, quantity=5, unit_price=20.0, total_price=100.0, payment_method='Cash')
+
+    def test_attributes(self):
+        self.assertEqual(self.sale.Sales_id, 1)
+        self.assertEqual(self.sale.buyer_name, 'John Doe')
+        self.assertEqual(self.sale.poultry_id, 101)
+        self.assertEqual(self.sale.quantity, 5)
+        self.assertEqual(self.sale.unit_price, 20.0)
+        self.assertEqual(self.sale.total_price, 100.0)
+        self.assertEqual(self.sale.payment_method, 'Cash')
+
+    def test_calculate_total_revenue(self):
+        self.assertEqual(self.sale.calculate_total_revenue(), 100.0)
+
+if __name__ == '__main__':
+    unittest.main()
