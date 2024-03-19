d2626b7fdb34d9bdf45315b12a8b6d6a78c4199a
owino600 <owinodaniel600@gmail.com>
2024-03-19 16:33:01 +0300

    updaes

--- models/base_model.py
+++ models/base_model.py
@@ -14,14 +14,13 @@ if models.storage_t == "db":
     Base = declarative_base()
 else:
     Base = object
-    
-    
-class BaseModel:
-    """The BaseModel class for poultry management"""
-    if models.storage_t == "db":
-        id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, default=datetime.utcnow)
-        updated_at = Column(DateTime, default=datetime.utcnow)
+
+    class BaseModel:
+        """The BaseModel class for poultry management"""
+        if models.storage_t == "db":
+            id = Column(String(60), primary_key=True)
+            created_at = Column(DateTime, default=datetime.utcnow)
+            updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
@@ -29,8 +28,10 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-            self.created_at = datetime.strptime(kwargs.get("created_at", datetime.utcnow().strftime(time_format)), time_format)
-            self.updated_at = datetime.strptime(kwargs.get("updated_at", datetime.utcnow().strftime(time_format)), time_format)
+            self.created_at = datetime.strptime(kwargs.get("created_at", datetime.utcnow()
+                .strftime(time_format)), time_format)
+            self.updated_at = datetime.strptime(kwargs.get("updated_at", datetime.utcnow()
+                .strftime(time_format)), time_format)
             self.id = kwargs.get("id", str(uuid.uuid4()))
         else:
             self.id = str(uuid.uuid4())
