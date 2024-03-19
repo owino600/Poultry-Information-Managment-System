bf3ef63e6e63461aed8cd48aae619f9dd2836881
254daniel <danielowino114@gmail.com>
2024-03-19 12:36:44 +0300

    updates

--- models/inventory.py
+++ models/inventory.py
@@ -8,11 +8,11 @@ from sqlalchemy import Column, String, Integer, DateTime, Float, Text
 
 class Inventory(BaseModel):
     __tablename__= 'product_sales'
-    Product_id = Column(Integer(60), primary_key=True, nullable=False)
+    Product_id = Column(Integer(), primary_key=True, nullable=False)
     Product_name = Column(String(60), nullable=False)
-    Category= Column(Integer(50), nullable=False)
+    Category= Column(Integer(), nullable=False)
     Description = Column(Text(200), nullable=True)
-    quantity = Column(Integer(50), nullable=False)
+    quantity = Column(Integer(), nullable=False)
     unit_price = Column(Float(50), nullable=False)
     supplier = Column(String(128), nullable=True)
     date_received = Column(DateTime, default=models.base_model.datetime.utcnow)
