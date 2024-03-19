f9e2f851a4a3037fe8d2b66daac57afeee81d4db
254daniel <danielowino114@gmail.com>
2024-03-19 13:21:30 +0300

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -28,14 +28,10 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-            # Convert string timestamps to datetime objects
-             # Convert string timestamps to datetime objects
             self.created_at = datetime.strptime(kwargs.get("created_at", datetime.utcnow().strftime(time_format)), time_format)
             self.updated_at = datetime.strptime(kwargs.get("updated_at", datetime.utcnow().strftime(time_format)), time_format)
-            # Generate a new UUID if not provided
             self.id = kwargs.get("id", str(uuid.uuid4()))
         else:
-            # Initialize with default values
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
@@ -47,13 +43,15 @@ class BaseModel:
     def save(self):
         """Updates the 'updated_at' attribute with the current datetime"""
         self.updated_at = datetime.utcnow()
+        models.storage.new(self)  # Add this line to call the new method of storage
 
     def to_dict(self):
         """Converts the instance to a dictionary"""
         return {
             "id": self.id,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at
+            "created_at": self.created_at.strftime(time_format),
+            "updated_at": self.updated_at.strftime(time_format),
+            "__class__": self.__class__.__name__
             # Add other attributes specific to your project
         }
 
