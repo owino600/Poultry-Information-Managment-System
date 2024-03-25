fee6d6c205c11822b9628608c748253ab6c5203a
root <root@DESKTOP-AL6U6H1>
2024-03-25 12:02:35 +0300

    console

--- console.py
+++ console.py
@@ -1,9 +1,38 @@
 #!/bin/usr/python3
 """ poultry management console"""
-import cmd
-import re
-import shlex
+def main():
+    while True:
+        print("Welcome to the Poultry Information Management System (PIMS)")
+        print("Please select an option:")
+        print("1. View Inventory")
+        print("2. View Sales")
+        print("3. View Medication Operations")
+        print("4. Exit")
 
-class PIMSCommand(cmd.Cmd):
-    """PIMS console comand"""
-    prompt = "PIMS"
+        option = input("> ")
+
+        if option == "1":
+
+            # Call the function to view inventory
+            view_inventory()
+        elif option == "2":
+            # Call the function to view sales
+            view_sales()
+        elif option == "3":
+            # Call the function to view medication operations
+            view_medication_operations()
+        elif option == "4":
+            break
+        else:
+            print("Invalid option. Please try again.")
+            def view_inventory():
+                # Use your Inventory class here
+                pass
+            def view_sales():
+                # Use your Sales class here
+                pass
+            def view_medication_operations():
+                # Use your Medication class here
+                pass
+            if __name__ == "__main__":
+                main()
