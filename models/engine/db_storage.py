a50ed1eadec48f6bb419240cfe51be93fe915b26
254daniel <danielowino114@gmail.com>
2024-03-26 16:56:36 +0300

    .\models\

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -26,7 +26,7 @@ class DBStorage:
                                         PIMS_MYSQL_PWD,
                                         PIMS_MYSQL_HOST,
                                         PIMS_MYSQL_DB,))
-        Base.metadata.create_all(self.__engine)
+        
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session()
