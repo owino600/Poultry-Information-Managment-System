278129ce921a9c6b4a3411b69d5e3ac3443b52d9
254daniel <danielowino114@gmail.com>
2024-03-26 16:14:30 +0300

    .\models\

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -27,10 +27,11 @@ class DBStorage:
                                         PIMS_MYSQL_HOST,
                                         PIMS_MYSQL_DB))
         
-        Base.metadata.create_all(self.__engine)
-        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sess_factory)
-        self.__session = Session()
+        if models.storage_t == "db":
+            Base.metadata.create_all(self.__engine)
+            sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+            Session = scoped_session(sess_factory)
+            self.__session = Session()
         
     def all(self, cls=None):
         """create query on the current database session"""
